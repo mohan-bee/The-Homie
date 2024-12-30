7967114396eff9064174253f437dc875fbc67f3b
dhaxanxxt <mohn08052006@gmail.com>
2024-12-30 15:32:46 +0530

    CORS

--- server/index.js
+++ server/index.js
@@ -13,13 +13,13 @@ if (!MONGO_URI) {
 }
 
 
-const corsOptions = {
-    origin: 'https://the-homie.vercel.app/',
-    methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
-};
+// const corsOptions = {
+//     origin: 'https://the-homie.vercel.app',
+//     methods: 'GET,POST,PUT,DELETE',
+//     allowedHeaders: 'Content-Type, Authorization',
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(express.json());
