d73e453ae30c7724870625b93db246cb542d5d7f
dhaxanxxt <mohn08052006@gmail.com>
2024-12-30 15:04:21 +0530

    Route Changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,22 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './Components/Home'
-import Editor from './Components/Editor'
-
-import './App.css'
-import Page from './Components/Page'
-import AdminPage from './Components/AdminPage'
+import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home';
+import Editor from './Components/Editor';
+import './App.css';
+import Page from './Components/Page';
+import AdminPage from './Components/AdminPage';
 
 const App = () => {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route index element={<Home/>} />
-      <Route path='post/:id' element={<Page/>} />
-      <Route path='/admin/new' element={<Editor/>} />
-      <Route path='/admin' element={<AdminPage/>} />
+    <HashRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="post/:id" element={<Page />} />
+        <Route path="admin/new" element={<Editor />} />
+        <Route path="admin" element={<AdminPage />} />
+      </Routes>
+    </HashRouter>
+  );
+};
 
-    </Routes>
-    </BrowserRouter>
-  )
-}
-
-export default App
+export default App;
